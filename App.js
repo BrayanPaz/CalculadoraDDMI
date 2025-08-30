0533a1399a34a99cff9fe9174b09b54b8a2c0d79
BrayanPaz <brayanpaz.bg016@academico.ifsul.edu.br>
2025-08-30 12:14:14 -0300

    Atualizar o App.js

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.calculadora}>
+        
+      </View>
     </View>
   );
 }
@@ -17,4 +18,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  calculadora: {
+    
 });
